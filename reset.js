16f481ecf65ebcac7781c8b7579c75b81ba205d9
IanR01 <ian.r.89@gmail.com>
2024-03-21 10:18:07 +0100

    add check for -f flag

--- reset.js
+++ reset.js
@@ -4,13 +4,16 @@ process.env.DEBUG = config.logging;
 const datafile = process.env.NODE_ENV || 'development';
 const db = require('./src/db');
 
+// check for force flag '-f' on commandline to force sync, like: node reset.js -f
+const force = process.argv.indexOf('-f') > -1
+
 async function doReset() {
 
   try {
 
     console.log('Syncing...');
 
-    await db.sequelize.sync({force: true})
+    await db.sequelize.sync({force})
 
     console.log('Adding default data...');
 	  await require(`./fixtures/${datafile}`)(db);
