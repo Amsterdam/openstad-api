4c9a6a3db2abf3ba6fecdc064137ca60b4fe36e1
IanR01 <IanR01@users.noreply.github.com>
2024-06-17 17:08:53 +0200

    Feature/restructure idea image urls (#18)
    * add restructure script
    
    * set old and new URLs as env vars
    
    * fix wrong path to db.js
    
    * import Op from sequelize
    
    * remove db.close
    
    * added logging
    
    * bypass hooks in the idea model
    
    * bypass validation on idea.save
    
    * close the db connection
    
    * add await to close db command
    
    * Explicitly set extraData field as changed for sequelize
    
    * more explicit comment
    
    * clean code
    
    * cleaner code
    
    * fix if statement for images in array check
    
    * fix 1 --> 0
    
    * implement correct isUpdated check
    
    * return when no db connection
    
    * typo startsWith
    
    * use .map to overwrit new image URLs

---
+++ scripts/restructure-ideas-image-urls.js
@@ -0,0 +1,52 @@
+const db = require('../src/db');
+
+async function updateImageUrls() {
+
+    let ideas = []
+
+    try {
+        ideas = await db.Idea.findAll();
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        return
+    }
+
+    console.log(`Image URL update process started for ${ideas.length} ideas (${process.env.IMAGE_URL_OLD_BASE_PATH} ==> ${process.env.IMAGE_URL_NEW_BASE_PATH})`)
+
+    for (let idea of ideas) {
+
+        let isUpdated = false
+        
+        if (!idea.extraData?.images?.length) {
+            continue
+        }
+
+        idea.extraData.images = idea.extraData.images.map(imageUrl => {
+            if (imageUrl.startsWith(process.env.IMAGE_URL_OLD_BASE_PATH)) {
+                isUpdated = true
+                return imageUrl.replace(process.env.IMAGE_URL_OLD_BASE_PATH, process.env.IMAGE_URL_NEW_BASE_PATH)
+            }
+            return imageUrl
+        })
+
+        if (!isUpdated) {
+            continue
+        }
+
+        idea.setDataValue('extraData', idea.extraData)
+        idea.changed('extraData', true);
+
+        try {
+            await idea.save({ hooks: false, validate: false }); // Bypass hooks and validation that otherwise will prevent the idea from saving
+            console.log(`Updated images for idea ID: ${idea.id}. The new image-URL's are: ${idea.extraData.images}`);
+        } catch (error) {
+            console.error(`Unable to save new image-URLs to the database for idea ${idea.id}: ${error}`)
+        }
+
+    }
+
+    console.log('Image URL update process completed.')
+
+}
+
+updateImageUrls();
