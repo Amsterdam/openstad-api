32699f06cfc4f5404b79bc526d8c69a235662873
IanR01 <ian@rieken.me>
2024-06-17 15:56:11 +0200

    implement correct isUpdated check

--- scripts/restructure-ideas-image-urls.js
+++ scripts/restructure-ideas-image-urls.js
@@ -13,20 +13,24 @@ async function updateImageUrls() {
     console.log(`Image URL update process started for ${ideas.length} ideas (${process.env.IMAGE_URL_OLD_BASE_PATH} ==> ${process.env.IMAGE_URL_NEW_BASE_PATH})`)
 
     for (let idea of ideas) {
+
+        let isUpdated = false
         
-        if (!idea.extraData?.images?.length > 0) {
+        if (!idea.extraData?.images?.length) {
             continue
         }
 
-        let updatedImageUrls = idea.extraData.images
-            .map(url => url.replace(process.env.IMAGE_URL_OLD_BASE_PATH, process.env.IMAGE_URL_NEW_BASE_PATH))
-
-        if (updatedImageUrls === idea.extraData.images) {
-            continue
+        for (let imageUrl of idea.extraData.images) {
+            if (imageUrl.startWith(process.env.IMAGE_URL_OLD_BASE_PATH)) {
+                imageUrl.replace(process.env.IMAGE_URL_OLD_BASE_PATH, process.env.IMAGE_URL_NEW_BASE_PATH)
+                isUpdated = true
+            }
         }
 
-        idea.extraData.images = updatedImageUrls
-
+        if (!isUpdated) {
+            continue
+        }
+        
         idea.setDataValue('extraData', idea.extraData)
         idea.changed('extraData', true);
 
@@ -36,6 +40,7 @@ async function updateImageUrls() {
         } catch (error) {
             console.error(`Unable to save new image-URLs to the database for idea ${idea.id}: ${error}`)
         }
+
     }
 
     console.log('Image URL update process completed.')
